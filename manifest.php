9dbccc5d6b6059863f595d239bdd8b57ff372e21
jonmlas <jonmlas@gmail.com>
2025-08-21 18:51:53 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -5,17 +5,27 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Breadcrumbs', 'fw' );
+$manifest['slug']        = 'unysonplus-breadcrumbs';
 $manifest['description'] = __(
-	'Creates a simplified navigation menu for the pages that can be placed anywhere in the theme.'
-	.' This will make navigating the website much easier.',
+	'Creates a simplified navigation menu for the pages that can be placed anywhere in the theme. This will make navigating the website much easier.',
 	'fw'
 );
-$manifest['version'] = '1.0.15';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Breadcrumbs-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/breadcrumbs/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Breadcrumbs-Extension';
+$manifest['version']     = '1.0.15';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Breadcrumbs-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Breadcrumbs-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
